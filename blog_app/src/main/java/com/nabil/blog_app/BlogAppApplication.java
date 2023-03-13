2f8fc648146776e2b12595fe780564ac8ea47ad5
nabil <n.oumellahni@gmail.com>
2023-03-13 17:34:09 +0100

    Blog App

--- blog_app/src/main/java/com/nabil/blog_app/BlogAppApplication.java
+++ blog_app/src/main/java/com/nabil/blog_app/BlogAppApplication.java
@@ -1,14 +1,29 @@
 package com.nabil.blog_app;
 
+import com.nabil.blog_app.entity.Role;
+import com.nabil.blog_app.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
-public class BlogAppApplication {
+public class BlogAppApplication implements CommandLineRunner {
+//    @Autowired
+//    private  RoleRepository roleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(BlogAppApplication.class, args);
     }
-
+    @Override
+    public void run(String... args) throws Exception {
+//        Role adminRole = new Role();
+//        adminRole.setName("ROLE_ADMIN");
+//        roleRepository.save(adminRole);
+//
+//        Role userRole = new Role();
+//        userRole.setName("ROLE_USER");
+//        roleRepository.save(userRole);
+    }
 }
